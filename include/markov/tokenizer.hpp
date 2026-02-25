b1f8639b16679c342c4f379282a47d60e3bfcb4f
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-02-25 15:49:05 +0000

    feat: Implemented a tokenizer

---
+++ include/markov/tokenizer.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <markov/types.hpp>
+
+namespace markov {
+  // Tokenize raw text into sequence of tokens.
+  //
+  // Rules(simeplified):
+  // - split on whitesapces
+  // - punctuation is treated as part of the tokens
+  // - no lowercasing here
+
+  std::vector<Token> tokenize ( const std::string& text );
+}
